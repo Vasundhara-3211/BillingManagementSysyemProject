6944c3ec41b4a86f5b4b4f2e468a79dc7a079f4e
Vasundhara Patil <vasundharapatil17@gmail.com>
2023-06-17 20:58:30 +0530

    second challenge

---
+++ Billing Managment System/src/billing/managment/system/BillingManagmentSystem.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package billing.managment.system;
+
+/**
+ *
+ * @author ASUS
+ */
+public class BillingManagmentSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       // System.out.print("hello");
+        // TODO code application logic here
+    }
+    
+}
